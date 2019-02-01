c852e8fd1e764bac0168ae4f8dbdd00ad69d5c93
Andreas <awunder@alpha-it-solutions.de>
2019-02-01 06:15:36 -0500

    add statistics

---
+++ lib/rocket_chat/statistics.rb
@@ -0,0 +1,31 @@
+module RocketChat
+  #
+  # Rocket.Chat Info
+  #
+  class Statistics
+    # Raw info data
+    attr_reader :data
+
+    #
+    # @param [Hash] data Raw info data
+    #
+    def initialize(data)
+      @data = data.dup.freeze
+    end
+
+    # Rocket.Chat version
+    def version
+
+      data['version']
+    end
+
+    def inspect
+      format(
+        '#<%<class_name>s:0x%<object_id>p @version="%<version>s">',
+        class_name: self.class.name,
+        object_id: object_id,
+        version: version
+      )
+    end
+  end
+end
